adf7433f0eeaa7a3bf8d7c96f01b484b3521dc5f
Andrew Templeton <atempleton@bright-channel.com>
2014-11-16 15:47:32 -0600

    Add in an ES connector healthcheck

--- app/endpoints/health/root.js
+++ app/endpoints/health/root.js
@@ -7,15 +7,36 @@
 
 
 var qpsTally = appRoot('middleware/qps');
+var http	 = require('http');
+
 
 module.exports = [
 	{
 		verb: 'GET',
 		path: '/',
 		handler: function(req, res) {
-			res.json({
-				qps: qpsTally.getCount()
+			var esReq = http.get(serviceConfigs.es, function(esRes) {
+				var chunks = '';
+				esRes.on('data', function(data) {
+					chunks += data.toString();
+				});
+				esRes.on('end', function() {
+					res.json({
+						qps: qpsTally.getCount(),
+						es: chunks
+					});
+				});
+				esRes.on('error', handleEsError);
 			});
+			esReq.on('error', handleEsError);
+
+			function handleEsError(err) {
+				res.statusCode = 500;
+				res.json({
+					qps: qpsTally.getCount(),
+					es: err.toString()
+				});
+			}
 		}
 	}
 ];
