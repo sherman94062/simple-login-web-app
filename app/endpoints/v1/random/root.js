89205c1a7a97594712b5a175637550ce2fa65fa2
Andrew Templeton <atempleton@bright-channel.com>
2014-11-22 02:34:06 -0600

    Add in random result endpoints

---
+++ app/endpoints/v1/random/root.js
@@ -0,0 +1,51 @@
+// app/endpoints/v1/random/root
+
+
+/*
+	Allows random access queries to indices.
+*/
+
+
+var Database = helper('db');
+
+var randomSeachQuery = {
+	query: {
+		function_score: {
+			query: {
+				match_all: {}
+			},
+			random_score: {}
+		}
+	}
+};
+
+module.exports = [{
+	verb: 'GET',
+	paths: ['/', '/:index', '/:index/:mapping'],
+	acceptParams: {
+		search_size: /^\d+$/,
+		size: 		 /^\d+$/
+	},
+	handler: function(req, res) {
+
+		var randomSearchPath = '/' + 
+			conditional(req.params.index) +
+			conditional(req.params.mapping) +
+			'_search?' + req.preppedQuery.split('&')[0];
+
+		Database.post(randomSearchPath,
+		randomSeachQuery,
+		function(err) {
+			res.statusCode = 404;
+			res.json({
+				message: '404 Not Found: Search scope not found.'
+			});
+		}, res);
+
+	}
+}];
+
+
+function conditional(fragment) {
+	return fragment ? (fragment + '/') : '';
+}
